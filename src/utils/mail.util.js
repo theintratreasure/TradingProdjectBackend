148821a07b206f472d98f63e111b87bb085d2126
Aman <aman.webdevlopment@gmail.com>
2026-07-31 11:38:50 +0530

    Respect SMTP secure transport setting

--- src/utils/mail.util.js
+++ src/utils/mail.util.js
@@ -22,7 +22,7 @@ const TEMPLATE_DIR = path.join(
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: String(process.env.SMTP_SECURE).toLowerCase() === 'true',
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
